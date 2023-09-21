67d703f1ede9342e108f81a13cf6cb7ef439c82c
MiguelAngelIF <144568771+MiguelAngelIF@users.noreply.github.com>
2023-09-20 20:12:27 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,16 +1,16 @@
 //entrada de los datos
 var items = [3,2,7,4,45,345,34,53,6,23,87,4];
 //definir la función
-function busqueda(items, elemento_a_buscar){
+function busqueda(items, posicion_a_buscar){
 var i = 0;
 var total_elementos_arreglo = items.length;
   while(i < total_elementos_arreglo){
-    if(items[i] === elemento_a_buscar){
-      return i;
+    if(items[i] === posicion_a_buscar){
+      return items[i];
     }
     i++;
   }
 }
 //salida buscar en el arreglo la posicion de un elemento
-var resultado = busqueda(items,45);
+var resultado = busqueda(items,posicion_a_buscar);
 console.log(resultado);
