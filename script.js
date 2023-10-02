6954dd7d36977c3681eee6821bb39be8714b8bda
MiguelAngelIF <144568771+MiguelAngelIF@users.noreply.github.com>
2023-10-01 20:53:57 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,25 +1,9 @@
-var galaxies = [3, 2, 56, 3, 78, 34];
+const numeros = [2,5,3,4,9,5,67,54];
+// multiplicar cada elemento del array por 10
+const arrayModificado = numeros.map(funcionMultiplicar)
 
-function funcionContarEstrellas(galaxies) {
-  var totalStars = galaxies.reduce(function (accumulator, stars) {
-    return accumulator + stars;
-  }, 0); // Agregar 0 como valor inicial del acumulador
-  return totalStars;
+function funcionMultiplicar(numero){
+  return numero * 10;
 }
 
-var resultado = funcionContarEstrellas(galaxies);
-
-document.write("Total de estrellas en las galaxias: " + resultado + "<br>");
-
-var i = 0;
-galaxias = Array.apply(null, Array(10000)).map(function () {
-  return ++i;
-});
-var count = 10000;
-var start = new Date();
-for (i = 0; i < count; i++) {
-  // Llama a la función countAllStart
-  funcionContarEstrellas(galaxias, 9999);
-}
-var miliseconds = new Date() - start;
-document.write("Tiempo en ms: " + miliseconds);
+document.querySelector('#header').innerHTML = arrayModificado;
