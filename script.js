594fbfd645fdf35501f5517986ebfb599e095b2b
MiguelAngelIF <144568771+MiguelAngelIF@users.noreply.github.com>
2023-10-02 20:57:35 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
-const numeros = [2,5,3,4,9,5,67,54];
+const manzanas = [2,5,3,4,9,5,67,54];
 // multiplicar cada elemento del array por 10
 const arrayModificado = numeros.map(funcionMultiplicar)
 
-function funcionMultiplicar(numero){
+function funcionMultiplicar(manzanas){
   return numero * 10;
 }
 
